93eb8016035ae2473d8bfe212588ac67e88430b0
Ryu <ultralmantigar@gmail.com>
2025-03-25 04:03:50 +0800

    添加了日志系统，增加了对特免金额一致性的判断

---
+++ main.py
@@ -0,0 +1,272 @@
+import shutil
+import os
+import logging
+from datetime   import datetime, date, timedelta
+from copy       import copy
+
+from openpyxl                import load_workbook
+from openpyxl.styles         import Font, Border, Side,  Alignment
+from openpyxl.cell.text      import InlineFont
+from openpyxl.cell.rich_text import CellRichText, TextBlock, TextBlock
+
+from meituan.main       import get_meituanSum, mt_status
+from douyin.main        import get_douyinSum
+from operation.main     import get_operation_data
+from operation.elecdata import get_elecUsage, get_row_by_date
+from specialFee      import main as specialFee
+from tools              import env
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+# 将 requests 库的日志级别设置为 DEBUG
+logging.getLogger('urllib3').setLevel(logging.DEBUG)
+
+working_datetime = env.work_datetime
+working_datetime_date  = working_datetime.date()
+
+logger.info(f'working date is: {working_datetime_date}')
+
+yesterday = date.today() - timedelta(days=1)
+
+if working_datetime_date != yesterday:
+    choosen = input(f"The target date is {working_datetime_date}, are you sure to continue? y/n \nOr type 'yesterday' to use yesterday: ") or "n"
+    if choosen == "y":
+        pass  # 继续执行
+    elif choosen == "yesterday":
+        working_datetime_date = yesterday
+    else:
+        # 如果在函数内，可以使用 return 退出；如果在脚本中，可以使用 sys.exit()
+        import sys
+        sys.exit("操作已取消。")
+
+dir_str     = f"{env.proj_dir}/et/{working_datetime_date.strftime('%m%d')}日报表"
+source_file = env.source_file
+save2file   = f"{dir_str}/2025年日报表.xlsx"
+
+mt = get_meituanSum()
+dy = get_douyinSum()
+english = get_operation_data()
+
+if mt_status == 1:
+    logger.debug("美团发生错误，检查cookie是否正确")
+
+
+elec = get_elecUsage()
+specialFee_list = specialFee.get_specialFee(reverse=False)        #可以排序
+
+manul_data = { "用电量":elec , "美团"  : mt , "抖音"  : dy }
+
+wb = load_workbook(source_file)
+ws = wb.active  # 或者指定具体的工作表，例如 wb["Sheet1"]
+
+def load_data():
+    chinese = {}
+    data_pure = {
+        "用电量" : elec ,
+        "美团"   : mt   ,
+        "抖音"   : dy
+    }
+
+    cn_en_map = {
+        "网费充值"  :     "amountFee",
+        "提现本金"  :     "withdrawPrincipal",
+        "找零"      :     "checkoutDeposit",
+        "零售"      :     "retail",
+        "水吧"      :     "waterBar",
+        "代购"      :     "agent",
+        "退款"      :     "totalRefundFee",
+        "报销"      :     None,
+        "在线支付"  :     "onlineIn",
+        "奖励金"    :     "awardFee",
+        "卡券"      :     "cardVolumeFee",
+        "特免"      :     "specialFree",
+        "网费消耗"  :     "totalConsumeNetworkFee",
+        "上机人次"  :     "onlineTimes",
+        "上机时长"  :     "duration",
+        "点单率"    :     "orderRate",
+        "新会员"    :     "newMember"
+    }
+
+    for cn_name, eng_name in cn_en_map.items():
+        if eng_name and eng_name in english:
+            data_pure[cn_name] = english[eng_name]
+        else:
+            data_pure[cn_name] = 0
+
+    data_pure["退款"]       = -data_pure["退款"] 
+    if data_pure['退款'] == 0 :
+        data_pure['退款'] = None
+
+    data_pure["上机时长"]   = round(data_pure["上机时长"] /60, 2)
+    return data_pure
+
+def insert_data(data_pure):
+    target_row = get_row_by_date(ws, working_datetime)
+    
+    logger.info(f'target_row is {target_row}')
+
+    for col in ws.iter_cols(min_row=2, max_row=2, min_col=1, max_col=29):
+        header = col[0].value  # 第二行的列标题
+        if header in data_pure:
+            ws.cell(row=target_row, column=col[0].column, value=data_pure[header])
+
+    date_str = working_datetime.strftime("%-m月%-d日")
+    #%-m 和 %-d 中的减号用于去除月份和日期中的前导零。请注意，这种用法在某些操作系统（如 Unix/Linux）上有效，但在 Windows 上可能不被支持。
+
+    ws["B36"].value = f"芜湖张家山店{date_str}营业状况"
+
+font_wenquan = Font(name='WenQuanYi Zen Hei',
+                size=11,
+                bold=False,
+                italic=False,
+                vertAlign=None,
+                underline='none',
+                strike=False,
+                color='FF000000'
+        )
+
+font_yahei = Font(name='Microsoft YaHei',
+                size= 8,
+                bold=False,
+                italic=False,
+                vertAlign=None,
+                underline='none',
+                strike=False,
+                color='FF000000'
+        )
+ws['G37'].font = font_yahei
+
+yahei_inline = InlineFont(rFont='Microsoft YaHei',
+                sz=11,
+                color='FF000000'
+        )
+yahei_22 = InlineFont(rFont='Microsoft YaHei',
+                sz=22,
+                color='FFFF0000'
+                    )
+yahei_red_8 = InlineFont(rFont='Microsoft YaHei',
+                sz=8,
+                color='FFFF0000'
+                    )
+b = TextBlock
+
+def special_mark(special_data):
+    left_alignment = Alignment(horizontal='left')
+    center_alignment = Alignment(horizontal='center')
+
+    thin    = Side(border_style="thin", color="888888")
+    medium  = Side(border_style="medium", color="000000")
+    dash    = Side(border_style="dashed", color="000000")
+    dotted  = Side(border_style="dotted", color="000000")
+
+    merged_ranges = ws.merged_cells.ranges
+
+    for row in range(37, 57):  # 37 到 57 行
+
+        merge_range_GI = f'G{row}:I{row}'
+        merge_range_GH = f'G{row}:H{row}'
+
+        # 检查 G{row}:I{row} 是否已被合并
+        if any(str(merged_range) == merge_range_GI for merged_range in merged_ranges):
+            ws.unmerge_cells(merge_range_GI)
+            ws.merge_cells(merge_range_GH)
+        left  = ws[f'H{row}']
+        right = ws[f'I{row}']
+        weft  = ws[f'G{row}']
+        weft.value  = None
+        right.value = None
+        weft.border  = Border(left = thin, bottom=dotted )
+        left.border  = Border(bottom=dotted)
+        right.border = Border(left=None, right=medium, bottom=dotted)
+        right.font  = copy(ws['G37'].font)
+        weft.font   = copy(ws['G37'].font)
+ 
+    bottom = 57
+    ws[f'G{bottom}'].border = Border(left=thin,bottom=medium)
+    ws[f'H{bottom}'].border = Border(bottom=medium)
+    ws[f'I{bottom}'].border = Border(bottom=medium,right=medium)
+
+    for index, item in enumerate(special_data):
+        row = 37 + index
+        if row > 56:
+            break  # 超过 G57，停止插入
+        reason, value  = item.split('：')
+
+        #rich_string = CellRichText(
+        #    b(yahei_inline, f'{reason}'),
+        #    b(yahei_red_8, f'{value}'),
+        #    b(yahei_red_8, 'sss')
+        #    )
+        cell = ws.cell(row=row, column=7)
+        cell.value = item
+        #cell = rich_string
+        cell.alignment = left_alignment
+
+        yuan = ws.cell(row=row, column=9)
+        yuan.value = "元"
+        yuan.alignment = center_alignment
+
+def A1Bug():
+    '''
+        A1的标题样式有bug，单独执行这个函数来处理
+    '''
+    red_font = Font(color="FF0000")
+    rich_text = CellRichText(
+        [
+            "网费收入",
+            TextBlock(InlineFont(red_font), "(网费+提现+找零)"),  # 使用 InlineFont 包装
+        ]
+    )
+    ws['A1'] = rich_text
+    font1 = Font(bold=True, size=16)
+    ws['A1'].font = font1
+def handle_headers(ws):
+    mcells = [ "f1", "j1", "n1", "s1", "w1", "ac1" ]
+
+    for cell in mcells:
+        text = ws[cell].value
+        start_index = text.find("(")
+        end_index = text.find(")")
+
+        if start_index != -1 and end_index != -1:
+            # 创建 Font 对象，设置字体大小
+            small_font = Font(size=8)
+            # 创建 CellRichText 对象
+            rich_text = CellRichText(
+                [
+                    text[:start_index],  # 括号前的文本
+                    TextBlock(InlineFont(small_font), text[start_index:end_index + 1]),  # 括号内的文本，小字
+                    text[end_index + 1:],  # 括号后的文本
+                ]
+            )
+        else:
+            # 如果没有括号，直接设置文本
+            rich_text = CellRichText([text])
+        ws[cell] = rich_text
+        A1Bug()
+
+def save(path):
+    try:
+        os.makedirs(dir_str, exist_ok=True)
+        print(f"目录 '{dir_str}' 创建成功")
+    except Exception as e:
+        print(f"创建目录时发生错误: {e}")
+    try:
+        shutil.move(source_file, f"{source_file}.old")
+        ws['Q28'].value = 'debullg'
+        wb.save(source_file)
+        wb.save(path)
+    except FileNotFoundError:
+        print(f"文件 {path} 或目录 {os.path.dirname(source_file)} 不存在")
+    except Exception as e:
+        print(f"复制文件 {path} 出错：{e}")
+
+#判断specialFeesum和specialFee是否一致I#
+data_pure = load_data()
+if data_pure['特免'] != specialFee.sum:
+    logger.warn(f"特免金额不匹配，请检查!!运营数据中是{data_pure['特免']},订单列表中计算出来是{speciaFee.sum}")
+
+insert_data(data_pure)
+special_mark(specialFee_list)
+handle_headers(ws)
+save(save2file)
